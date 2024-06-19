f8e33f035653612f0efe6d7267703ae955da72b3
drewbauer <bauean4@gmail.com>
2024-06-19 12:27:17 -0500

    Changed button function to if else to remove image and change button when clicked

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ var cat = document.querySelector(".cat");
 //console.log(cat.outerHTML);
 
 button.addEventListener("click", function () {
-  //console.log("It's Working.");
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    //console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Here, kitty, kitty!";
+    button.classList.add("disappear");
+  } else {
+    //console.log("No");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+  }
 });
